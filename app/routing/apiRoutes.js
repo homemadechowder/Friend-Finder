e1c12a33e97871f3139f66c54b4e1376e5013b72
Andrew Lu <awclu@ucdavis.edu>
2020-01-08 23:22:53 -0800

    commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,29 @@
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        return res.json(characters);
+    });
+  
+
+    app.post("/api/friends", function(req, res) {
+      console.log("Survey Entry Data:");
+      console.log(req.body);
+      // req.body hosts is equal to the JSON post sent from the user
+      // This works because of our body parsing middleware
+        var newSurvey = req.body;
+
+      // Using a RegEx Pattern to remove spaces from newCharacter
+      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        newSurvey.routeName = newSurvey.name.replace(/\s+/g, "").toLowerCase();
+
+        console.log(newSurvey);
+
+        characters.push(newSurvey);
+
+        res.json(newSurvey);
+    });
+  
+    
+};
+
+  
